0e10a7168f63da7f3741c1502d7499ba6628265c
Rosa Truneh <rosa.truneh@gmail.com>
2021-08-25 20:02:11 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ import Register from "views/examples/Register.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact render={props => <Index {...props} />} />
+      <Route path="/how-to" exact render={props => <Index {...props} />} />
       <Route
         path="/landing-page"
         exact
--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -108,47 +108,43 @@ class Profile extends React.Component {
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
                         <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
+                          <span className="heading">333</span>
+                          <span className="description">Frenimies</span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
-                          <span className="description">Photos</span>
+                          <span className="heading">777</span>
+                          <span className="description">Pics</span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
+                          <span className="heading">999</span>
+                          <span className="description">What They Said</span>
                         </div>
                       </div>
                     </Col>
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Rosa Truneh{" "}
+                      <span className="font-weight-light">, 34</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Nashville, TN
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Entrepreneur
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      University of Middle Tennessee
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                          A current bookkeeper learning coding to grown myself into an Entrepreneur.
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
